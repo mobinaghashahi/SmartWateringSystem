6cc57b421a2f644f412656a297c06842f80a8987
mobin <mobinaghashahi13@gmail.com>
2026-06-26 19:21:09 +0330

    create vesion controller for APIs

--- routes/api.php
+++ routes/api.php
@@ -6,78 +6,81 @@ use App\Models\State;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::prefix('v1')->group(function () {
 //get token by uuid for devices request
-Route::post('/get_token', function (Request $request) {
-    $devices = Devices::where('uuid', $request->uuid)->first();
+    Route::post('/get_token', function (Request $request) {
+        $devices = Devices::where('uuid', $request->uuid)->first();
 
-    if (!$devices) {
+        if (!$devices) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $customer_name = $devices->customer_name;
+
+        //delete previous tokens
+        $devices->tokens()->delete();
+        $token = $devices->createToken($customer_name);
+
         return response()->json([
-            'message' => 'Invalid credentials'
-        ], 401);
-    }
+            'token' => $token->plainTextToken
+        ]);
+    });
 
-    $customer_name = $devices->customer_name;
 
-    //delete previous tokens
-    $devices->tokens()->delete();
-    $token = $devices->createToken($customer_name);
+    Route::prefix('watering')
+        ->middleware('auth:sanctum')
+        ->group(function () {
+            Route::post('/get_last_status', function (Request $request) {
+                $state = State::where('uuid', $request->uuid)->orderBy('command_date', 'desc')->first();
+                return response()->json([
+                    'state' => $state
+                ]);
+            });
 
-    return response()->json([
-        'token' => $token->plainTextToken
-    ]);
+            Route::post('/get_history', function (Request $request) {
+                $state = State::where('uuid', $request->uuid)->get();
+                return response()->json([
+                    'states' => $state
+                ]);
+            });
+
+            Route::post('/turn_off', function (Request $request) {
+                $request->validate([
+                    'uuid' => 'required|string|exists:devices,uuid',
+                ]);
+                $uuid = $request->uuid;
+                $state = new State();
+                $state->watering_state = 0;
+                $state->uuid = $uuid;
+                $state->save();
+
+                return response()->json([
+                    'success' => true
+                ]);
+            });
+
+            Route::post('/turn_on', function (Request $request) {
+                $request->validate([
+                    'uuid' => 'required|string|exists:devices,uuid',
+                ]);
+                $uuid = $request->uuid;
+                $state = new State();
+                $state->watering_state = 1;
+                $state->uuid = $uuid;
+                $state->save();
+
+                return response()->json([
+                    'success' => true
+                ]);
+            });
+        });
+
+
+    Route::post('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 });
-
-
-Route::prefix('watering')
-    ->middleware('auth:sanctum')
-    ->group(function () {
-    Route::post('/get_last_status', function (Request $request) {
-        $state = State::where('uuid', $request->uuid)->orderBy('command_date', 'desc')->first();
-        return response()->json([
-            'state' => $state
-        ]);
-    });
-
-    Route::post('/get_history', function (Request $request) {
-        $state = State::where('uuid', $request->uuid)->get();
-        return response()->json([
-            'states' => $state
-        ]);
-    });
-
-    Route::post('/turn_off', function (Request $request) {
-        $request->validate([
-            'uuid' => 'required|string|exists:devices,uuid',
-        ]);
-        $uuid = $request->uuid;
-        $state = new State();
-        $state->watering_state = 0;
-        $state->uuid = $uuid;
-        $state->save();
-
-        return response()->json([
-            'success' => true
-        ]);
-    });
-
-    Route::post('/turn_on', function (Request $request) {
-        $request->validate([
-            'uuid' => 'required|string|exists:devices,uuid',
-        ]);
-        $uuid = $request->uuid;
-        $state = new State();
-        $state->watering_state = 1;
-        $state->uuid = $uuid;
-        $state->save();
-
-        return response()->json([
-            'success' => true
-        ]);
-    });
-});
-
-
-Route::post('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 

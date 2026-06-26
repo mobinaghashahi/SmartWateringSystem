1f4e061205ed90d621a1fd1e50f366b5a353ff31
mobin <mobinaghashahi13@gmail.com>
2026-06-26 15:08:00 +0330

    define turn_off_watering and turn_on_watering APIs

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,15 @@ use Illuminate\Support\Facades\Route;
 
 //get token by uuid for devices request
 Route::post('/get_token', function (Request $request) {
-    $devices=Devices::where('uuid', $request->uuid)->first();
+    $devices = Devices::where('uuid', $request->uuid)->first();
 
-    if (! $devices) {
+    if (!$devices) {
         return response()->json([
             'message' => 'Invalid credentials'
         ], 401);
     }
 
-    $customer_name=$devices->customer_name;
+    $customer_name = $devices->customer_name;
 
     //delete previous tokens
     $devices->tokens()->delete();
@@ -28,16 +28,46 @@ Route::post('/get_token', function (Request $request) {
 });
 
 Route::post('/get_last_watering_status', function (Request $request) {
-    $state=State::where('uuid', $request->uuid)->orderBy('command_date', 'desc')->first();
+    $state = State::where('uuid', $request->uuid)->orderBy('command_date', 'desc')->first();
     return response()->json([
-        'state'=>$state
+        'state' => $state
     ]);
 })->middleware('auth:sanctum');
 
 Route::post('/get_last_watering_history', function (Request $request) {
-    $state=State::where('uuid', $request->uuid)->get();
+    $state = State::where('uuid', $request->uuid)->get();
     return response()->json([
-        'states'=>$state
+        'states' => $state
+    ]);
+})->middleware('auth:sanctum');
+
+Route::post('/turn_off_watering', function (Request $request) {
+    $request->validate([
+        'uuid' => 'required|string|exists:devices,uuid',
+    ]);
+    $uuid=$request->uuid;
+    $state = new State();
+    $state->watering_state = 0;
+    $state->uuid = $uuid;
+    $state->save();
+
+    return response()->json([
+        'success' => true
+    ]);
+})->middleware('auth:sanctum');
+
+Route::post('/turn_on_watering', function (Request $request) {
+    $request->validate([
+        'uuid' => 'required|string|exists:devices,uuid',
+    ]);
+    $uuid=$request->uuid;
+    $state = new State();
+    $state->watering_state = 1;
+    $state->uuid = $uuid;
+    $state->save();
+
+    return response()->json([
+        'success' => true
     ]);
 })->middleware('auth:sanctum');
 
@@ -46,7 +76,7 @@ Route::post('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('/test', function (Request $request) {
-    return ['name'=>[
+    return ['name' => [
         'ali',
         'hassan'
     ]];

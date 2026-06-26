532f4453d3c260d14e6e37ecaeda512a46f02927
mobin <mobinaghashahi13@gmail.com>
2026-06-26 14:57:15 +0330

    change state tables schema

--- database/migrations/2026_06_24_133540_states.php
+++ database/migrations/2026_06_24_133540_states.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('state', function (Blueprint $table) {
+        Schema::create('states', function (Blueprint $table) {
             $table->id();
             $table->boolean('watering_state')->default(0);
             $table->timestamp('command_date');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('state');
+        Schema::dropIfExists('states');
     }
 };
